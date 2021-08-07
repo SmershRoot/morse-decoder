33c4dcbbb599fb57a81bc589f8542cc6f0b18bbf
Smersh <Smrsh.Root@gmail.com>
2021-08-07 17:30:51 +0300

    realese task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = "";
+    for(let startChar=0; expr.length>startChar;startChar+=10){
+        let subExpr=expr.substr(startChar, 10);
+        if(subExpr == '**********'){
+            result+=' ';
+            continue;
+        }
+        let charMarze =""; 
+        for(let start=0; subExpr.length>start;start+=2){
+            if(subExpr.substr(start,2)=='10'){
+                charMarze+='.';
+            }
+            if(subExpr.substr(start,2)=='11'){
+                charMarze+='-';
+            }
+        }
+        result+=MORSE_TABLE[charMarze];
+    }
+    return result;
 }
 
 module.exports = {
